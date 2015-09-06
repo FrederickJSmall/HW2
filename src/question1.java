ea7a120879dd138efc72d0d5e018ad964b1d7a72
Fred Small <Fsmall@startoffsmall.com>
2015-09-06 18:22:32 -0400

    added new function to calculate the pyramid.

--- src/question1.java
+++ src/question1.java
@@ -7,13 +7,38 @@ public class question1 {
 		Scanner keyboard = new Scanner(System.in);
 
 		System.out.println("How tall do you want the pyramid");
-		int height = keyboard.nextInt();
+		//int height = keyboard.nextInt();
+		int height = 3;
 		int width = ((height-1)) * 2 + 1;
 		int middle = height;//(width/2)-1;
 		
 		System.out.println("Width=" + width + ",Height=" + height+ ",Middle=" + middle );
 		System.out.println("");
 		System.out.println("Calculating");
+		
+		question1 question = new question1();
+		for (int i=1;i<=height;i++)
+		{
+			question.buildPyramid(i);
+		}
+		/*
+		for (int z=0;z<height;z++)
+		{
+		String[] pyramid2 = new String[width];
+		pyramid2[height-1] = 0 + 1 + "";
+		for (int x = 0;x<middle;x++)
+		{
+			pyramid2[x] = x + 1 + "";
+			pyramid2[x+middle-1] = middle -x + "";
+		}
+		for (int x = 0;x<width;x++)
+		{
+			System.out.printf(" %s", pyramid2[x]);
+		}
+		System.out.println("");
+		}
+		*/
+		/*
 		for (int i = 0;i<height;i++)
 		{
 			String[] pyramid = new String[width];
@@ -22,37 +47,42 @@ public class question1 {
 			{
 				pyramid[x] = x + 1 + "";
 				pyramid[x+middle-1] = middle -x + "";
-				//System.out.println(middle-x);
-				//pyramid[middle-x] = middle-x + "";
-				//pyramid[x] = x + 1 + "";
-			}
-			//for (int x = middle+1;x<height;x++)
-			//{
-			//	pyramid[x] = x + 1 + "";
-			//	//pyramid[x] = x + 1 + "";
-			//}
-			for (int x = 0;x<width;x++)
-			{
-				System.out.printf(" %s", pyramid[x]);
-			}
-			/*
-			for (int x = 0;x<middle;x++)
-			{
-				pyramid[x] = x + 1 + "";
-				pyramid[x] = x + 1 + "";
 			}
 
-			*/
+			//print row
+			for (int x = 0;x<width;x++)
+			{
+				
+				System.out.printf(" %s", pyramid[x]);
+			}
+
 			System.out.println("");
-			//pyramid[height-1] = i + 1 + "";
-			///System.out.println(pyramid[height-1]);
-			//for (int j=0;j<width;j++)
-			//{
-			//}
 		}
-		
+		*/
 
 		System.out.println("We are done");
 		keyboard.close();
+	}
+	public void buildPyramid(int height)
+	{
+		int width = ((height-1)) * 2 + 1;
+		int middle = height;//(width/2)-1;
+		
+		//for (int z=0;z<height;z++)
+		//{
+		String[] pyramid2 = new String[width];
+		pyramid2[height-1] = 0 + 1 + "";
+		for (int x = 0;x<middle;x++)
+		{
+			pyramid2[x] = x + 1 + "";
+			pyramid2[x+middle-1] = middle -x + "";
+		}
+		for (int x = 0;x<width;x++)
+		{
+			System.out.printf(" %s", pyramid2[x]);
+		}
+		System.out.println("");
+		//}
+		
 	}
 }
