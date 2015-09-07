c6951286e700f2d1afae9166ce03b2935e99a827
Fred Small <Fsmall@startoffsmall.com>
2015-09-06 20:26:29 -0400

    checkpoint save

--- src/question1.java
+++ src/question1.java
@@ -19,7 +19,7 @@ public class question1 {
 		question1 question = new question1();
 		for (int i=1;i<=height;i++)
 		{
-			question.buildPyramid(i);
+			question.buildPyramid(i,width,middle);
 		}
 		/*
 		for (int z=0;z<height;z++)
@@ -63,20 +63,49 @@ public class question1 {
 		System.out.println("We are done");
 		keyboard.close();
 	}
-	public void buildPyramid(int height)
+	public void buildPyramid(int height,int width,int middle)
 	{
-		int width = ((height-1)) * 2 + 1;
-		int middle = height;//(width/2)-1;
+		//int width = ((height-1)) * 2 + 1;
+		//int middle = height;//(width/2)-1;
 		
 		//for (int z=0;z<height;z++)
 		//{
 		String[] pyramid2 = new String[width];
-		pyramid2[height-1] = 0 + 1 + "";
-		for (int x = 0;x<middle;x++)
+		pyramid2[middle-1] = height + "";
+		for (int x = 1;x<middle;x++)
 		{
-			pyramid2[x] = x + 1 + "";
-			pyramid2[x+middle-1] = middle -x + "";
+			//pyramid2[x] = x + 1 + "";
+			//pyramid2[x+middle-1] = middle -x + "";
+			//String x2 = pyramid2[(middle-x)-1];
+			int value=0;
+			String strValue = pyramid2[(middle-1)];
+			if (isNumber(strValue))
+			{
+				value = Integer.parseInt(strValue)-x;
+				//System.out.println(value);
+			}
+			else 
+				value = 0;
+			//String value = pyramid2[(middle-x)-1];
+
+			
+			//int value = 0;
+			//try
+			//{
+			//  value = Integer.parseInt(pyramid2[(middle-x)-1])-1;
+			//}
+			//catch (Exception ex)
+			//{
+			//	value = 0;
+			//}
+			
+			pyramid2[middle-x-1] = value + "";
+			pyramid2[middle+x-1] = value + "";
 		}
+		//for (int x = 0;x<width;x++)
+		//{
+		//	System.out.printf(" %s", pyramid2[x]);
+		//}
 		for (int x = 0;x<width;x++)
 		{
 			System.out.printf(" %s", pyramid2[x]);
@@ -84,5 +113,13 @@ public class question1 {
 		System.out.println("");
 		//}
 		
+	}
+	private static boolean isNumber(String value) {
+	    try {
+	        Integer.parseInt(value);
+	        return true;
+	    } catch (NumberFormatException nfe) {
+	        return false;
+	    }
 	}
 }
